a9706f5352c00e7a437c5e183d626fd47c5b23b2
Ryan Holmes <holmes.ryan.m@gmail.com>
2020-08-09 07:56:01 +1000

    Add ocean_month to processing.

--- ncra_proc.sh
+++ ncra_proc.sh
@@ -7,7 +7,11 @@ OUTP=010
 
 echo "ncra ocean.nc"
 ncra archive/output${OUTP}/ocean/ocean.nc archive/processed/ocean.${NAME}.out${OUTP}.ncra.nc
+echo "ncra ocean_month.nc"
+ncra archive/output${OUTP}/ocean/ocean_month.nc archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.nc
 echo "ncrcat ocean_scalar.nc"
 ncrcat archive/output*/ocean/ocean_scalar.nc archive/processed/ocean_scalar.${NAME}.out000-${OUTP}.ncrcat.nc
 echo "ncdiff ocean.ncra.nc"
 ncdiff archive/processed/ocean.${NAME}.out${OUTP}.ncra.nc /scratch/e14/rmh561/access-om2/archive/1deg_jra55_ryf_red3DSK/processed/ocean.cont.out${OUTP}.ncra.nc archive/processed/ocean.${NAME}.out${OUTP}.ncra.diff.nc
+echo "ncdiff ocean_month.ncra.nc"
+ncdiff archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.nc /scratch/e14/rmh561/access-om2/archive/1deg_jra55_ryf_red3DSK/processed/ocean_month.cont.out${OUTP}.ncra.nc archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.diff.nc
