d20f32cef95dbc1139af9044bbcc8a905c819cd2
Ryan Holmes <holmes.ryan.m@gmail.com>
2020-08-24 22:12:24 +1000

    2020-08-24 22:12:24: Run 30

--- ncra_proc.sh
+++ ncra_proc.sh
@@ -3,7 +3,7 @@
 module load nco
 
 NAME=C6_2D_MLT
-OUTP=010
+OUTP=029
 
 echo "ncra ocean.nc"
 ncra archive/output${OUTP}/ocean/ocean.nc archive/processed/ocean.${NAME}.out${OUTP}.ncra.nc
@@ -11,7 +11,12 @@ echo "ncra ocean_month.nc"
 ncra archive/output${OUTP}/ocean/ocean_month.nc archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.nc
 echo "ncrcat ocean_scalar.nc"
 ncrcat archive/output*/ocean/ocean_scalar.nc archive/processed/ocean_scalar.${NAME}.out000-${OUTP}.ncrcat.nc
-echo "ncdiff ocean.ncra.nc"
-ncdiff archive/processed/ocean.${NAME}.out${OUTP}.ncra.nc /scratch/e14/rmh561/access-om2/archive/1deg_jra55_ryf_red3DSK/processed/ocean.cont.out${OUTP}.ncra.nc archive/processed/ocean.${NAME}.out${OUTP}.ncra.diff.nc
-echo "ncdiff ocean_month.ncra.nc"
-ncdiff archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.nc /scratch/e14/rmh561/access-om2/archive/1deg_jra55_ryf_red3DSK/processed/ocean_month.cont.out${OUTP}.ncra.nc archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.diff.nc
+echo "ncra -v mld_max -y max ocean_month.nc"
+ncra -v mld_max -y max archive/output${OUTP}/ocean/ocean_month.nc archive/processed/ocean_month.mld_max.${NAME}.out${OUTP}.ncra.nc
+
+
+
+# echo "ncdiff ocean.ncra.nc"
+# ncdiff archive/processed/ocean.${NAME}.out${OUTP}.ncra.nc /scratch/e14/rmh561/access-om2/archive/1deg_jra55_ryf_red3DSK/processed/ocean.cont.out${OUTP}.ncra.nc archive/processed/ocean.${NAME}.out${OUTP}.ncra.diff.nc
+# echo "ncdiff ocean_month.ncra.nc"
+# ncdiff archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.nc /scratch/e14/rmh561/access-om2/archive/1deg_jra55_ryf_red3DSK/processed/ocean_month.cont.out${OUTP}.ncra.nc archive/processed/ocean_month.${NAME}.out${OUTP}.ncra.diff.nc
