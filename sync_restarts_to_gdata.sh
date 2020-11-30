ef145510e31e5cd5495dbf9f797d510111f2f172
Ryan Holmes <holmes.ryan.m@gmail.com>
2020-12-01 10:46:42 +1100

    Update sync scripts.

--- sync_restarts_to_gdata.sh
+++ sync_restarts_to_gdata.sh
@@ -3,7 +3,8 @@
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -l walltime=2:00:00,mem=2GB
-#PBS -P v45
+#PBS -P e14
+#PBS -l storage=gdata/e14+scratch/e14
 #PBS -N restarts_to_gdata
 
 source sync_output_to_gdata.sh # to define GDATADIR and cd archive
